9c5897ab020a3a9c9aec8994040279552de94118
mukesh mishra <mishramukesh556@gmail.com>
2020-09-19 01:54:53 +0400

    commit

---
+++ main.js
@@ -0,0 +1,86 @@
+
+/* var fs=require('fs');
+var data =fs.readFileSync('text.txt');
+console.log(data.toString());
+console.log('end here'); */
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+   res.sendfile('index.html');
+});
+var internetAvailable = require("internet-available");
+
+internetAvailable().then(function () {
+   console.log("Internet available");
+}).catch(function () {
+   console.log("No internet");
+});
+
+
+
+
+var users = [];
+var left=[];
+
+io.on('connection', (socket) => {
+
+   console.log('user connected' + ':' + socket.id);
+   socket.emit('message', 'welcome to our chat room');
+
+
+  
+
+   socket.on('new-user-join', name => {
+
+      socket.emit('message', 'welcome' + ':' + name);
+      socket.emit('heading', 'welcome' + ':' + name);
+     io.emit('auser',  name);
+
+      console.log(name + ':' + 'connected');
+     
+
+      users[name]=socket.id;
+      left[socket.id]=name;
+
+      io.emit('user-join', name);
+
+   });
+
+   socket.on('chatm',data => {
+      var socketid=users[data.receiver];
+
+       io.to(socketid).emit('pmsg',  data);
+    
+      
+
+   });
+   socket.on('chatp',data => {
+socket.broadcast.emit('pumsg',data);
+
+   });
+
+   socket.on('disconnect', message => {
+     socket.broadcast.emit('left',left[socket.id]);
+     socket.broadcast.emit('leftli', left[socket.id]);
+    
+    
+    
+      console.log(left[socket.id] +':' +  'disconnected');
+      delete left[socket.id];
+    });
+  
+
+  
+
+});
+
+
+
+
+http.listen(3000, () => {
+   console.log('listening on *:3000');
+});
+
